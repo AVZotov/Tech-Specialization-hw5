b52f65e59ed5dfcd3090b9705c36e0d3cd269dc3
Alexey Zotov <Alexey.Zotov@ymail.com>
2024-03-03 12:22:36 +0300

    Task 02 done

---
+++ src/main/java/ru/geekbrains/task02/Program.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.task02;
+
+import java.io.File;
+
+public class Program {
+    public static void main(String[] args) {
+        tree(new File("."));
+    }
+
+    public static void tree(File folder) {
+        int indent = 0;
+        StringBuilder sb = new StringBuilder();
+        printDirectory(folder, indent, sb);
+        System.out.println(sb);
+    }
+
+    private static void printDirectory(File folder, int indent, StringBuilder sb) {
+        sb.append(getIndentString(indent)).append("└─").append(folder.getName()).append("/").append("\n");
+        File[] files = folder.listFiles();
+
+        if (files == null){
+            return;
+        }
+
+        for (File file : files) {
+            if (file.isDirectory()) {
+                printDirectory(file, indent + 1, sb);
+            } else {
+                printFile(file, indent + 1, sb);
+            }
+        }
+    }
+
+    private static void printFile(File file, int indent, StringBuilder sb) {
+        sb.append(getIndentString(indent)).append("▪ ").append(file.getName()).append("\n");
+    }
+
+    private static String getIndentString(int indent) {
+        return "|  ".repeat(Math.max(0, indent));
+    }
+}
+
